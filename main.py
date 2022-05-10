f187b1bb14439a5359f9a1444e7c135b9462ddd0
Artem Babak <artbbk27@gmail.com>
2022-05-10 20:21:05 +0300

    refactor

--- main.py
+++ main.py
@@ -10,30 +10,82 @@ from telebot import types
 
 bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
+
+# ---------------- Текст ----------------------------------------
+
+RESET_LEARNING = 'Учить числа с нуля'
+
+ADD_NUMBERS = 'Добавить новые числа'
+
+LIST = 'Посмотреть изучаемые числа'
+
+LEARN = 'Учить'
+
+KNOW = 'Знаю'
+
+DONT_KNOW = 'Не знаю'
+
+END = 'Закончить'
+
+END_BACK_TO_MENU = 'Молодец! Ты хорошо поучил(а)! Выбери что хочешь сделать'
+
+INVALID_INPUT = 'Ты ввел что-то не то, попробуй еще раз или перепроверь что ввод корректный'
+
+INPUT_NUMBERS = 'Введи числа, которые ты хочешь учить в подобном формате:\n<b>1-5,8,10,12-20,13</b>'
+
+NO_MORE_NUMBERS = 'Я тебя больше ничему не могу научить...'
+
+NUMBERS_ADDED = 'Успешно добавлено'
+
+START_MESSAGE = f'Привет! Я научу тебя правильно произносить числа.\n' \
+                f'Если ты хочешь сбросить свой прогресс и изучаемые числа нажми <b>{RESET_LEARNING}</b>\n' \
+                f'Если ты хочешь добавить новые числа к уже изучаемым нажми <b>{ADD_NUMBERS}</b>\n' \
+                f'Если ты хочешь посмотреть список изучаемых чисел нажми <b>{LIST}</b>\n' \
+                f'Если ты хочешь приступить к изучению нажми <b>{LEARN}</b>\n' \
+                f'Если что, ты всегда можешь ввести <b>/start</b> или <b>/help</b>,' \
+                f'чтобы снова отобразить это сообщение и вернуться в начало'
+
+
+# ---------------------Клавиатура---------------------------
+
 def create_common_markup(one_time=False):
+    # Создаем клавиатуру
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=one_time)
-    start_learning_btn = types.KeyboardButton('Учить числа c нуля')
-    learn_btn = types.KeyboardButton('Учить')
-    add_numbers_btn = types.KeyboardButton('Добавить новые числа')
-    number_list_btn = types.KeyboardButton('Посмотреть изучаемые числа')
+    
+    # Создаем кнопки
+    start_learning_btn = types.KeyboardButton(RESET_LEARNING)
+    learn_btn = types.KeyboardButton(LEARN)
+    add_numbers_btn = types.KeyboardButton(ADD_NUMBERS)
+    number_list_btn = types.KeyboardButton(LIST)
+    
+    # Добавляем кнопки в клавиатуру в два ряда
     markup.add(start_learning_btn, learn_btn)
     markup.add(add_numbers_btn, number_list_btn)
     return markup
 
 
 def create_learn_markup():
+    # Создаем клавиатуру
     markup = types.ReplyKeyboardMarkup()
-    know_btn = types.KeyboardButton('Знаю')
-    dont_know_btn = types.KeyboardButton('Не знаю')
-    stop_learn_btn = types.KeyboardButton('Закончить')
+
+    # Создаем кнопки
+    know_btn = types.KeyboardButton(KNOW)
+    dont_know_btn = types.KeyboardButton(DONT_KNOW)
+    stop_learn_btn = types.KeyboardButton(END)
+    
+    # Добавляем кнопки в клавиатуру в два ряда
     markup.add(know_btn, dont_know_btn)
     markup.add(stop_learn_btn)
     return markup
 
-
+# Создаем клавиатуры и сохраняем
 common_markup = create_common_markup()
 learn_markup = create_learn_markup()
+# Эту клавиатуру указываем чтобы спрятать текущую и показать обычную
 hide_markup = types.ReplyKeyboardRemove()
+
+
+# ---------------------Object Storage---------------------------
 
 s3 = boto3.resource(
     's3',
@@ -46,36 +98,17 @@ s3 = boto3.resource(
 json.load_s3 = lambda f: json.load(s3.Object(key=f).get()["Body"])
 json.dump_s3 = lambda obj, f: s3.Object(key=f).put(Body=json.dumps(obj))
 
-# ---------------- dialog params ----------------
-START_MESSAGE = "Привет! Я научу тебя правильно произносить числа. " \
-                "Пока я могу только скинуть тебе правильное произношение чиссел, но " \
-                "скоро я смогу также проверять твое произношение!\n\n" \
-                "Команды:\n" \
-                "/start - Приветствие\n" \
-                "/help - Список команд\n" \
-                "/start_learning - Начать учить числа с нуля\n" \
-                "/add_numbers - Добавить числа к изучению\n" \
-                "/learn - прислать произношение случайного числа из выбранных\n" \
-                "/list - список чисел для изучения"
 
-START_LEARNING = "Начать учить числа с нуля\n"
-
-
-# ----------------------some functions------------
+# ----------------------Просто функции--------------------------
 def isNumber(s):
-    # handle for negative values
     negative = False
     if (s[0] == '-'):
         negative = True
-
     if negative == True:
         s = s[1:]
-
-    # try to convert the string to int
     try:
         n = int(s)
         return True
-    # catch exception if cannot be converted
     except ValueError:
         return False
 
@@ -100,117 +133,138 @@ def resetNumbers(chatId):
     json.dump_s3(data, "data.json")
 
 
-# --------------------- bot ---------------------
-
-@bot.message_handler(commands=['help', 'start'])
-def say_welcome(message):
-    bot.send_message(message.chat.id, START_MESSAGE, reply_markup=common_markup)
-
-
-@bot.message_handler(commands=['start_learning'])
-def start_learning(message):
-    print('start_learning')
-    msg = bot.send_message(message.chat.id, "Введи числа, которые ты хочешь учить в подобном формате:\n"
-                                            "1-5,8,10,12-20,13", reply_markup=hide_markup)
-    resetNumbers(message.chat.id)
-    bot.register_next_step_handler(msg, add_numbers)
-
-
-@bot.message_handler(commands=['learn'])
-def learn(message):
-    print('learn')
-    data = json.load_s3("data.json")
-    print(data)
-    lowest_category = "?"
-    for category in ["0", "1", "2", "3", "4", "5"]:
-        if data[str(message.chat.id)]["numbers"][category]:
-            lowest_category = category
-            break
-    if lowest_category == "?":
-        bot.send_message(message.chat.id, "Я тебя больше ничему не могу научить...", reply_markup=common_markup)
-    category = lowest_category
-    numbers = data[str(message.chat.id)]["numbers"][category]
-    number = numbers[randint(0, len(numbers) - 1)]
+def getFpOfSynthesizedNumber(number):
     tts = gTTS(text=str(number), lang="en")
     fp = BytesIO()
     tts.write_to_fp(fp)
     fp.seek(0)
-    data[str(message.chat.id)]["last_number"] = number
-    data[str(message.chat.id)]["last_category"] = category
+    return fp
+# --------------------- Бот ---------------------
+
+@bot.message_handler(commands=['help', 'start'])
+def say_welcome(message):
+    bot.send_message(message.chat.id, START_MESSAGE, reply_markup=common_markup, parse_mode="HTML")
+
+
+@bot.message_handler(commands=['start_learning'])
+def start_learning(message):
+    msg = bot.send_message(message.chat.id, INPUT_NUMBERS, reply_markup=hide_markup, parse_mode="HTML")
+    resetNumbers(message.chat.id)
+    bot.register_next_step_handler(msg, add_numbers)
+
+
+# FIXME #5
+@bot.message_handler(commands=['learn'])
+def learn(message):
+    data = json.load_s3("data.json")
+    chatId = str(message.chat.id)
+    
+    # Найти минимальную категории, где есть числа
+    # Оттуда и будем выбирать число
+    lowest_category = ""
+    for category in ["0", "1", "2", "3", "4", "5"]:
+        if data[chatId]["numbers"][category]:
+            lowest_category = category
+            break
+    if lowest_category == "":
+        bot.send_message(message.chat.id, NO_MORE_NUMBERS, reply_markup=common_markup)
+    
+    # Выбираем число из выбранной категории
+    numbers = data[chatId]["numbers"][lowest_category]
+    number = numbers[randint(0, len(numbers) - 1)]
+
+    # Озвучиваем число
+    fp = getFpOfSynthesizedNumber(number)
+
+    # Сохраняем число и категорию чтобы после 
+    # ответа знаю/не знаю переместить нужное число
+    # в нужную категорию
+    data[chatId]["last_number"] = number
+    data[chatId]["last_category"] = lowest_category
+    
     json.dump_s3(data, "data.json")
+    
     bot.send_message(message.chat.id, number, reply_markup=learn_markup)
     bot.send_voice(message.chat.id, fp)
 
 def add_numbers(message):
-    print('add_numbers')
     data = json.load_s3("data.json")
-    data[str(message.chat.id)]["numbers"]["0"] += getNumberList(message.text)
-    category_0_set = set(data[str(message.chat.id)]["numbers"]["0"])
+    chatId = str(message.chat.id)
+    
+    # Добавляем числа в нулевую категорию и убираем повторы
+    data[chatId]["numbers"]["0"] += getNumberList(message.text)
+    category_0_set = set(data[chatId]["numbers"]["0"])
     for category in ["1", "2", "3", "4", "5", "6"]:
-        category_0_set -= set(data[str(message.chat.id)]["numbers"][category])
-    data[str(message.chat.id)]["numbers"]["0"] = list(category_0_set)
+        category_0_set -= set(data[chatId]["numbers"][category])
+    data[chatId]["numbers"]["0"] = list(category_0_set)
+    
     json.dump_s3(data, "data.json")
-    bot.send_message(message.chat.id, "Успешно добавлено", reply_markup=common_markup)
+    
+    bot.send_message(message.chat.id, NUMBERS_ADDED, reply_markup=common_markup)
 
 
 @bot.message_handler(commands=['add_numbers'])
 def add_numbers_handler(message):
-    print("add_numbers_handler")
-    msg = bot.send_message(message.chat.id, "Введи числа, которые ты хочешь учить в подобном формате:\n"
-                                            "1-5,8,10,12-20,13", reply_markup=hide_markup)
+    msg = bot.send_message(message.chat.id, INPUT_NUMBERS, reply_markup=hide_markup, parse_mode="HTML")
     bot.register_next_step_handler(msg, add_numbers)
 
 
 @bot.message_handler(commands=['list'])
 def number_list(message):
-    print("number_list")
     data = json.load_s3("data.json")
+    chatId = str(message.chat.id)
+    
     numbers = []
     for category in ["0", "1", "2", "3", "4", "5", "6"]:
-        numbers += data[str(message.chat.id)]["numbers"][category]
+        numbers += data[chatId]["numbers"][category]
     numbers.sort()
+    
     bot.send_message(message.chat.id, str(numbers), reply_markup=common_markup)
 
 
 def know(message):
-    print('know')
     data = json.load_s3("data.json")
-    last_category = data[str(message.chat.id)]["last_category"]
-    last_number = data[str(message.chat.id)]["last_number"]
-    data[str(message.chat.id)]["numbers"][last_category].remove(last_number)
-    data[str(message.chat.id)]["numbers"][str(min(int(last_category) + 1, 6))].append(last_number)
+    chatId = str(message.chat.id)
+    
+    last_category = data[chatId]["last_category"]
+    last_number = data[chatId]["last_number"]
+    data[chatId]["numbers"][last_category].remove(last_number)
+    data[chatId]["numbers"][str(min(int(last_category) + 1, 6))].append(last_number)
+    
     json.dump_s3(data, "data.json")
 
 def dont_know(message):
-    print('dont_know')
     data = json.load_s3("data.json")
-    last_category = data[str(message.chat.id)]["last_category"]
-    last_number = data[str(message.chat.id)]["last_number"]
-    data[str(message.chat.id)]["numbers"][last_category].remove(last_number)
-    data[str(message.chat.id)]["numbers"][str(max(int(last_category) - 1, 0))].append(last_number)
+    chatId = str(message.chat.id)
+    
+    last_category = data[chatId]["last_category"]
+    last_number = data[chatId]["last_number"]
+    data[chatId]["numbers"][last_category].remove(last_number)
+    data[chatId]["numbers"][str(max(int(last_category) - 1, 0))].append(last_number)
+    
     json.dump_s3(data, "data.json")
 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text == 'Учить числа c нуля':
+    if message.text == RESET_LEARNING:
         start_learning(message)
-    elif message.text == 'Учить':
+    elif message.text == LEARN:
         learn(message)
-    elif message.text == 'Добавить новые числа':
+    elif message.text == ADD_NUMBERS:
         add_numbers_handler(message)
-    elif message.text == 'Посмотреть изучаемые числа':
+    elif message.text == LIST:
         number_list(message)
-    elif message.text == 'Знаю':
+    elif message.text == KNOW:
         know(message)
         learn(message)
-    elif message.text == 'Не знаю':
+    elif message.text == DONT_KNOW:
         dont_know(message)
         learn(message)
-    elif message.text == 'Закончить':
-        bot.send_message(message.chat.id, "Молодец! Ты хорошо поучил(а)! Выбери что хочешь сделать", reply_markup=common_markup)
+    elif message.text == END:
+        bot.send_message(message.chat.id, END_BACK_TO_MENU, reply_markup=common_markup)
     else:
-        bot.send_message(message.chat.id, "что то странное", reply_markup=common_markup)
+        bot.send_message(message.chat.id, INVALID_INPUT, reply_markup=common_markup)
 
 
 # ---------------- local testing ----------------
